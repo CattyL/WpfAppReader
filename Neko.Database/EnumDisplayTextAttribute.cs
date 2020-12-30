d078bf879b9db0416a3e73a7eb5a730a135b605a
CattyL <1191214560@qq.com>
2020-12-30 11:23:45 +0800

    .

---
+++ Neko.Database/EnumDisplayTextAttribute.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection;
+
+namespace Neko.Database
+{
+    /// <summary>
+    /// 展示名称特性
+    /// </summary>
+    public class EnumDisplayTextAttribute : Attribute
+    {
+        /// <summary>
+        /// 展示的名称
+        /// </summary>
+        public string DisplayText { get; private set; }
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="displayText">展示的名称</param>
+        public EnumDisplayTextAttribute(string displayText)
+        {
+            DisplayText = displayText;
+        }
+        /// <summary>
+        /// 获得value的类型名称
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="nameInstead"></param>
+        /// <returns></returns>
+        public static string GetDispText(object value, bool nameInstead = true)
+        {
+            Type type = value.GetType();
+            string name = Enum.GetName(type, value);
+            if (name == null)
+            {
+                return "";
+            }
+
+            FieldInfo field = type.GetField(name);
+
+            EnumDisplayTextAttribute attribute = GetCustomAttribute(field, typeof(EnumDisplayTextAttribute)) as EnumDisplayTextAttribute;
+            if (attribute == null && nameInstead == true)
+            {
+                return name;
+            }
+            return attribute == null ? null : attribute.DisplayText;
+        }
+    }
+}
---
+++ Neko.Database/eDatabaseType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neko.Database
+{
+    /// <summary>
+    /// 数据库类型
+    /// </summary>
+    public enum eDatabaseType
+    {
+        [EnumDisplayText("SqlServer数据库")]
+        SqlServer,
+        [EnumDisplayText("Oracle数据库")]
+        Oracle,
+        [EnumDisplayText("SQLite数据库")]
+        SQLite,
+        [EnumDisplayText("MySql数据库")]
+        MySql,
+        [EnumDisplayText("无")]
+        None
+    }
+}
